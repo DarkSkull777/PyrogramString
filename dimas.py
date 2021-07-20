c1b82b59559f9f12b6845c4f8ef6f9915aab342d
Dimas Ramadani <59839225+DarkSkull777@users.noreply.github.com>
2021-07-20 09:56:29 +0700

    update dimas.py

--- dimas.py
+++ dimas.py
@@ -3,11 +3,11 @@ from pyrogram import Client as c
 API_ID = input("\nMasukin API_ID syg:\n > ")
 API_HASH = input("\nAhhhh Crooottt Sekarang API_HASH eghmm:\n > ")
 
-print("\n\n Ahhhh Keluarnya Banyak Bgtt, Lagi Dong Masukin Nomer Telepon Pakai Kode Negara:\n\n")
+print("\n\n Dimas Gantengg\n\n")
 
 i = c(":memory:", api_id=API_ID, api_hash=API_HASH)
 
 with i:
     ss = i.export_session_string()
-    print("\nAHHHH Udah nanti aku bisa hamilll, Di atas string kamu salin aja crot!!\n")
+    print("\nAHHHH CROTT!! String Kamu Ada Di Bawah Salin Aja.\n")
     print(f"\n{ss}\n")
